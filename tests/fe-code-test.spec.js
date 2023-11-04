5863296891b851cd0826365b90eeb5c3f6dc2aef
Amanda <apita1423@outlook.com>
2023-11-04 22:25:59 +0000

    Start test case 3 with filling in invalid email

--- tests/fe-code-test.spec.js
+++ tests/fe-code-test.spec.js
@@ -11,15 +11,21 @@ test('test ekco navigation', async({ page }) => {
 
     // check login button and form
     await page.getByRole('link', { name: 'Log in' }).click();
-
     await page.goto("https://portal.ek.co/login?next=");
     await expect(page).toHaveURL("https://portal.ek.co/login?next=");
     await expect(page.locator('.col-md-4')).toBeVisible();
     await page.pause();
 
-    //Test Case 2 - Form Errors - I reused the portal links from above for the form errors test
+    // Test Case 2 - Form Errors - Reused the portal links from above for the form errors test
+    // check if login button is clicked first that email and password would show required
     await page.getByRole('button', { name: 'Log In' }).click();
     await expect(page.getByText('Email is required')).toBeVisible();
-    await expect(page.getByText('Email is required')).toBeVisible();
+    await expect(page.getByText('Password is required')).toBeVisible();
+    await page.pause();
+
+    //Test Case 3 - Invalid Email Error
+    await page.getByPlaceholder('Your email').fill('mail.com');
     await page.pause();
 });
+
+
