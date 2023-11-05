e4e1dc22be46ff0b67aabe2d2037e0a89a255812
Amanda <apita1423@outlook.com>
2023-11-05 08:50:31 +0000

    Finish test case 3 invalid email error

--- tests/fe-code-test.spec.js
+++ tests/fe-code-test.spec.js
@@ -7,25 +7,25 @@ test('test ekco navigation', async({ page }) => {
 
     // check page title text is visible
     await expect(page.getByRole('heading', { name: 'The people who power your possible' })).toBeVisible();
-    await page.pause();
 
-    // check login button and form
+    // check after login button is clicked that the url and login form is visible 
     await page.getByRole('link', { name: 'Log in' }).click();
-    await page.goto("https://portal.ek.co/login?next=");
+    await page.goto("https://portal.ek.co/");
     await expect(page).toHaveURL("https://portal.ek.co/login?next=");
     await expect(page.locator('.col-md-4')).toBeVisible();
-    await page.pause();
 
-    // Test Case 2 - Form Errors - Reused the portal links from above for the form errors test
-    // check if login button is clicked first that email and password would show required
+// Test Case 2 - Form Errors - Reused the portal links from above for ther continuation of forms
+    // check when login button is clicked first email and password would show required
     await page.getByRole('button', { name: 'Log In' }).click();
     await expect(page.getByText('Email is required')).toBeVisible();
     await expect(page.getByText('Password is required')).toBeVisible();
-    await page.pause();
 
-    //Test Case 3 - Invalid Email Error
+//Test Case 3 - Invalid Email Error
+    await page.goto("https://portal.ek.co/");
     await page.getByPlaceholder('Your email').fill('mail.com');
-    await page.pause();
+    await page.getByRole('button', { name: 'Log In' }).click();
+    await expect(page.getByText('Email must be a valid email')).toBeVisible();
+    await page.getByPlaceholder('Your email').fill('email@mail.com');
+    await page.getByRole('button', { name: 'Log In' }).click();
+    await expect(page.getByText('Email must be a valid email')).toBeHidden();
 });
-
-
